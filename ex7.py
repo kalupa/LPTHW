825cc09881fcc58b4e396521f7e0c00c68a9a175
kalupa <paul@karrupa.com>
2011-09-12 16:34:06 +0900

    update to ex7

--- ex7.py
+++ ex7.py
@@ -17,5 +17,5 @@ end11 = "e"
 end12 = "r"
 
 # watch that comma at the end. try removing it to see what happens
-print end1 + end2 + end3 + end4  + end5  + end6
+print end1 + end2 + end3 + end4  + end5  + end6,
 print end7 + end8 + end9 + end10 + end11 + end12
